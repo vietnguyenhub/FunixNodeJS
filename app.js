54874fc44cf41955de73a88dc10a5db26643cfca
vietnguyxn <vietnguyenhht@gmail.com>
2022-03-11 17:47:50 +0700

    lab01_4

---
+++ app.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My first page</title></head>");
+  res.write("<body><h1>Hello From my Nodejs sever</h1></body>");
+  res.write("</html>");
+  res.end();
+});
+
+server.listen(3000);
