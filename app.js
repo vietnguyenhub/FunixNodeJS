54bdc367d168e1867f40f78d0d4376360bea9e12
vietnguyxn <vietnguyenhht@gmail.com>
2022-03-11 20:43:23 +0700

    lab01_5

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
@@ -12,7 +13,12 @@ const server = http.createServer((req, res) => {
     res.write("</html>");
     return res.end();
   }
-
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.text", "DUMMNY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My first page</title></head>");
