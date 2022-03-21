8250b2c0e326eeefe1006c1237da26ed06d37816
vietnguyxn <vietnguyenhht@gmail.com>
2022-03-21 10:01:11 +0700

    lab2.4

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const http = require("http");
-
 const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("in the middlewave");
-  next(); // Allows the request to continue to the next middlewave is line;
+app.use("/", (req, res, next) => {
+  console.log("This always runes!");
+  next();
 });
 
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
+  console.log("to the middlewave");
+  res.send('<h1>The "Add Product" Page</h1>');
+});
+app.use("/", (req, res, next) => {
   console.log("to the middlewave");
   res.send("<h1>Hello from Express</h1>");
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
