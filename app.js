1a10bbe2214af38f0cc873d786cfb53fa1020a0b
vietnguyxn <vietnguyenhht@gmail.com>
2022-03-21 09:00:31 +0700

    lab2.2

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("in the middlewave");
+  next(); // Allows the request to continue to the next middlewave is line;
+});
+
+app.use((req, res, next) => {
+  console.log("to the middlewave");
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
