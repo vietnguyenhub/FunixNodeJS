d9fcefaa176cc4f846920435c48843046fef5597
vietnguyxn <vietnguyenhht@gmail.com>
2022-03-31 15:33:25 +0700

    lab4.7

--- models/product.js
+++ models/product.js
@@ -1,27 +1,32 @@
 const fs = require("fs");
 const path = require("path");
+
 const p = path.join(
   path.dirname(process.mainModule.filename),
   "data",
   "products.json"
 );
+
 const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
-      return cb([]);
+      cb([]);
     } else {
       cb(JSON.parse(fileContent));
     }
-    cb(JSON.parse(fileContent));
   });
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
+    this.id = Math.random().toString();
     getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
